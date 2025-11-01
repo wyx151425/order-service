6903801ab22bf9e6abcead48f81c2d19236d4fed
wyx151425 <wyx151425@163.com>
2025-11-01 10:47:52 +0800

    feat: 添加WebClient反应式客户端

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -18,29 +18,33 @@ import java.time.Duration;
 @Component
 public class BookClient {
 
-	private static final String BOOKS_ROOT_API = "/books/";
+    private static final String BOOKS_ROOT_API = "/books/";
 
     private final WebClient webClient;
 
-	public BookClient(WebClient webClient) {
+    public BookClient(WebClient webClient) {
         // 前文配置的 WebClient bean
-		this.webClient = webClient;
-	}
+        this.webClient = webClient;
+    }
 
-	public Mono<Book> getBookByIsbn(String isbn) {
-		return webClient
+    public Mono<Book> getBookByIsbn(String isbn) {
+        return webClient
                 // 请求应该使用 GET 方法
-				.get()
-				// 请求的目标 URL 是 /books/{isbn}
+                .get()
+                // 请求的目标 URL 是 /books/{isbn}
                 .uri(BOOKS_ROOT_API + isbn)
-				// 发送请求并获取响应
+                // 发送请求并获取响应
                 .retrieve()
                 // 以 Mono<Book> 的形式返回要检索的对象
-				.bodyToMono(Book.class)
-				.timeout(Duration.ofSeconds(3), Mono.empty())
-				.onErrorResume(WebClientResponseException.NotFound.class, exception -> Mono.empty())
-				.retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
-				.onErrorResume(Exception.class, exception -> Mono.empty());
-	}
+                .bodyToMono(Book.class)
+                // 为 GET 请求设置 3 秒的超时，回退行为会返回一个空的 Mono 对象
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                // 当接收到 404 响应时，返回一个空的对象
+                .onErrorResume(WebClientResponseException.NotFound.class, exception -> Mono.empty())
+                // 使用指数退避作为重试策略。允许重试 3 次并且初始延迟为 100 毫秒
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+                // 如果在三次重试后，依然出现错误，则捕获异常并返回空对象
+                .onErrorResume(Exception.class, exception -> Mono.empty());
+    }
 
 }
--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.orderservice.order.domain;
 
+import com.polarbookshop.orderservice.book.Book;
+import com.polarbookshop.orderservice.book.BookClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,9 +16,11 @@ import reactor.core.publisher.Mono;
 @Service
 public class OrderService {
 
+    private final BookClient bookClient;
     private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(BookClient bookClient, OrderRepository orderRepository) {
+        this.bookClient = bookClient;
         this.orderRepository = orderRepository;
     }
 
@@ -26,10 +30,22 @@ public class OrderService {
     }
 
     public Mono<Order> submitOrder(String isbn, int quantity) {
-        // 基于 Order 对象创建 Mono
-        return Mono.just(buildRejectedOrder(isbn, quantity))
-                // 将上一步通过反应式流生成的 Order 对象保存到数据库中
+//        // 基于 Order 对象创建 Mono
+//        return Mono.just(buildRejectedOrder(isbn, quantity))
+//                // 将上一步通过反应式流生成的 Order 对象保存到数据库中
+//                .flatMap(orderRepository::save);
+        return bookClient.getBookByIsbn(isbn)
+                // 如果图书可用，则接受该订单
+                .map(book -> buildAcceptedOrder(book, quantity))
+                // 如果图书不可用，则拒绝该订单
+                .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
+                // 保存订单 (可能是 accepted 状态或 rejected 状态)
                 .flatMap(orderRepository::save);
+    }
+
+    public static Order buildAcceptedOrder(Book book, int quantity) {
+        // 当订单被接受时，指定 ISBN、图书名称 (书名+作者)、数量和状态。Spring Data 会负责添加标识符，版本和审计元数据
+        return Order.of(book.isbn(), book.title() + " - " + book.author(), book.price(), quantity, OrderStatus.ACCEPTED);
     }
 
     public static Order buildRejectedOrder(String bookIsbn, int quantity) {
