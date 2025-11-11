e4f49b2c1a42ebdd5bf43ba882a94b0c0d7f77e2
wyx151425 <wyx151425@163.com>
2025-11-11 21:39:57 +0800

    test: 添加安全性测试

--- src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
@@ -1,14 +1,17 @@
-package com.polarbookshop.orderservice.order.web.web;
+package com.polarbookshop.orderservice.order.web;
 
+import com.polarbookshop.orderservice.config.SecurityConfig;
 import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
 import com.polarbookshop.orderservice.order.domain.OrderStatus;
-import com.polarbookshop.orderservice.order.web.OrderController;
-import com.polarbookshop.orderservice.order.web.OrderRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
@@ -23,6 +26,8 @@ import static org.mockito.BDDMockito.given;
  */
 // 标识该测试类主要关注 Spring WebFlux 组件，具体来讲，针对的是 OrderController
 @WebFluxTest(OrderController.class)
+// 导入应用的安全配置
+@Import(SecurityConfig.class)
 class OrderControllerWebFluxTests {
 
     // 具有额外特性的 WebClient 变种，会使 RESTful 服务的测试更简便
@@ -33,6 +38,10 @@ class OrderControllerWebFluxTests {
     @MockBean
     private OrderService orderService;
 
+    // Mock ReactiveJwtDecoder，这样为了解码访问令牌，应用就不需要尝试调用 Keycloak 以获取公钥了
+    @MockBean
+    ReactiveJwtDecoder reactiveJwtDecoder;
+
     @Test
     void whenBookNotAvailableThenRejectOrder() {
         var orderRequest = new OrderRequest("1234567890", 3);
@@ -42,6 +51,10 @@ class OrderControllerWebFluxTests {
                 .willReturn(Mono.just(expectedOrder));
 
         webClient
+                .mutateWith(SecurityMockServerConfigurers
+                        // 修改 HTTP 请求，使其包含一个 JWT 格式的 mock 访问令牌，该令牌具有 “customer” 角色
+                        .mockJwt()
+                        .authorities(new SimpleGrantedAuthority("ROLE_customer")))
                 .post()
                 .uri("/orders")
                 .bodyValue(orderRequest)
